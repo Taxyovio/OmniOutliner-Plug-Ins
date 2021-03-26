937545aeb29d0358b9f4e2ff8fb1729415f6e1c1
Tachyon <taxyovio@cantab.net>
2021-03-26 08:18:03 +0000

    Fix duplicateColumn renaming in splitColumn.js

--- format.omnioutlinerjs/Resources/splitColumn.js
+++ format.omnioutlinerjs/Resources/splitColumn.js
@@ -24,26 +24,11 @@ var _ = function(){
 			if (column.title !== ''){
 				return column.title
 			} else if (column === document.outline.noteColumn){
-			// The note column has empty title for unknown reason
+			// The note column is the only text column with empty title
 				return 'Notes'
 			}
 		})
-		// Rename columns with the same titles temporarily to work around bugs in Timer and Tree.paste
-		if (hasDuplicates(filteredColumnTitles)) {
-			var duplicateColumns = {"columns":[], "titles":[]}
-			filteredColumnTitles = renameStrings(filteredColumnTitles)
-			filteredColumns.forEach((column,index) => {
-				if (column.title !== ''){
-					if (column.title !== filteredColumnTitles[index]) {
-						duplicateColumns.columns.push(column)
-						duplicateColumns.titles.push(column.title)
-						column.title = filteredColumnTitles[index]
-					}
-				}
-			})
-		}
 		
-
 		
 		// CREATE FORM FOR GATHERING USER INPUT
 		var inputForm = new Form()
@@ -80,6 +65,25 @@ var _ = function(){
 			var selectedColumn = formObject.values["columnInput"]
 			var selectedColumnTitle = selectedColumn.title
 			
+			// Rename columns with the same titles temporarily to work around bugs in Timer and Tree.paste
+			if (hasDuplicates(filteredColumnTitles)) {
+				var duplicateColumns = {"columns":[], "titles":[], "indices":[]}
+				filteredColumnTitles = renameStrings(filteredColumnTitles)
+				filteredColumns.forEach((column,index) => {
+					if (column.title !== ''){
+						if (column.title !== filteredColumnTitles[index]) {
+							duplicateColumns.columns.push(column)
+							duplicateColumns.titles.push(column.title)
+							duplicateColumns.indices.push(columns.indexOf(column))
+							column.title = filteredColumnTitles[index]
+						}
+					}
+				})
+				var duplicateColumnTitles = duplicateColumns.titles
+				var duplicateColumnIndices = duplicateColumns.indices
+			}
+			
+			
 			selection.nodes.forEach(function(node, index){
 				editor.copyNodes([node],pb)
 				// Get the text object from topic column
@@ -97,26 +101,52 @@ var _ = function(){
 				var paragraphArrayLength = paragraphStringArray.length
 				
 				if (paragraphArrayLength > 1) {
+					// Prepare timer to work around Tree.paste bug
 					var counter = 0
 					var repeats = paragraphArrayLength
-					//node.object.valueForColumn(selectedColumn).string = paragraphStringArray.slice().reverse()[0]
-					// In the Timer, all objects get invalidated except the document object.
+					
+					// In the Timer, all user defined objects get invalidated. 
+					// Usable objects: document, columns, rootItem, outlineColumn, noteColumn, statusColumn
 					Timer.repeating(0, function(timer){
 						var node = document.editors[0].selection.nodes[index]
 						if (selectedColumnTitle === '') {
 								var column = document.outline.noteColumn
 							} else {
-								var column = document.outline.columns.byTitle(selectedColumnTitle)
+								var editor = document.editors[0]
+								
+								var filteredColumns = columns.filter(function(column){
+									if (editor.visibilityOfColumn(column)){
+										if (column.type === Column.Type.Text){return column}
+									}
+								})
+								
+								
+								var filteredColumnTitles = filteredColumns.map(function(column){
+									if (column.title !== ''){
+										return column.title
+									} else if (column === document.outline.noteColumn){
+									// The note column is the only text column with empty title
+										return 'Notes'
+									}
+								})
+								var column = columnByTitle(filteredColumns, selectedColumnTitle)
 							}
 							
 						if (counter === repeats){
+							if (duplicateColumnTitles !== undefined && duplicateColumnTitles !== null) {
+								duplicateColumnTitles.forEach((title, i) => {
+									columns[duplicateColumnIndices[i]].title = title
+								})
+							}
+							
 							console.log('done')
 							timer.cancel()
 						} else if (counter === 0) {
+							console.log('counter: ', counter)
 							node.object.valueForColumn(column).string = paragraphStringArray.slice().reverse()[0]
 							counter = counter + 1
 						} else {
-							console.log('counter:', counter)
+							console.log('counter: ', counter)
 							var childIndex = node.index
 							document.editors[0].paste(pb, node.parent, childIndex)
 							var newNode = node.parent.children[childIndex]
@@ -132,11 +162,6 @@ var _ = function(){
 					alert.show()
 				}
 			})
-			
-			duplicateColumns.columns.forEach((column, index) => {
-				column.title = duplicateColumns.titles[index]
-			})
-			
 		})
 		
 		// PROMISE FUNCTION CALLED UPON FORM CANCELLATION
@@ -161,7 +186,7 @@ function renameStrings(arr){
 		if (arr.indexOf(x) !== i) {
 			var c = x in count ? count[x] = count[x] + 1 : count[x] = 1
 			var j = c + 1
-			var k = x + ' '.repeat(j)
+			var k = x + ' '.repeat(j) + j
 			while(arr.indexOf(k) !== -1) k = x + ' ' + (++j)
 			arr[i] = k
 		}
@@ -171,4 +196,12 @@ function renameStrings(arr){
 
 function hasDuplicates(array) {
 	return (new Set(array)).size !== array.length;
+}
+
+function columnByTitle(columnArray, title) {
+	for (var i = 0; i < columnArray.length; i++) {
+		if (columnArray[i].title === title) {
+			return columnArray[i]
+		}
+	}
 }
