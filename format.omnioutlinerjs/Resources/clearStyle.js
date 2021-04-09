0bcb278341fe93dbd545a044a2998f3ae9766518
Tachyon <taxyovio@cantab.net>
2021-04-09 19:54:35 +0100

    Clear text styles in clearStyle.js

--- format.omnioutlinerjs/Resources/clearStyle.js
+++ format.omnioutlinerjs/Resources/clearStyle.js
@@ -4,15 +4,22 @@ var _ = function(){
 	var action = new PlugIn.Action(function(selection, sender) {
 		// action code
 		// selection options: columns, document, editor, items, nodes, styles
+		var textColumns = columns.filter(col => {return col.type === Column.Type.Text})
+		
 		selection.items.forEach(function(item){
-			item.style.setStyle(document.outline.baseStyle)
+			item.style.clear()
+			if (textColumns.length !== 0) {
+				textColumns.forEach(col => {
+					try {item.valueForColumn(col).style.clear()} catch (error) {}
+				})
+			}
 		})
 	});
 
 	action.validate = function(selection, sender) {
 		// validation code
 		// selection options: columns, document, editor, items, nodes, styles
-		if(selection.items.length > 0){return true} else {return false}
+		if (selection.items.length > 0) {return true} else {return false}
 	};
 	
 	return action;
